a949b35d9b3316de4950e5e2cd0d9e1dcb819088
Akshat Bubna <akshatb42@gmail.com>
2021-11-29 00:13:54 -0800

    update poller to filter out all new posts (instead of just last date)

--- src/datomic/ion/rustic/poller.clj
+++ src/datomic/ion/rustic/poller.clj
@@ -3,6 +3,8 @@
                                [clojure.xml :as xml]
                                [datomic.client.api :as d]
                                [datomic.ion.rustic.schema :as schema]
+                               [datomic.ion.rustic.utils :as u]
+                               [clj-time.core :as t]
                                [clj-time.format :as tf]
                                [clj-time.coerce :as tc]))
 
@@ -11,43 +13,56 @@
 
 (def rss-formatter (tf/formatters :rfc822))
 
-(defn find-rss-last-date [xml-content]
-  (->> xml-content
+(defn is-recent-post [last-updated-date post-date]
+  (or (nil? last-updated-date) (== -1 (compare last-updated-date post-date))))
+
+(defn rss-post-date [c]
+  (->> c
        :content
-       first :content
-       (find-tag :item)
-       first :content
        (find-tag :pubDate)
        first :content
        first
        (tf/parse rss-formatter)
        tc/to-date))
 
-(defn find-atom-last-date [xml-content]
+(defn rss-new-posts [xml-content last-updated-date]
   (->> xml-content
        :content
-       (find-tag :entry)
        first :content
+       (find-tag :item)
+       (filter #(is-recent-post last-updated-date (rss-post-date %)))))
+
+(defn atom-post-date [c]
+  (->> c
+       :content
        (find-tag :published)
        first :content
        first
        instant/read-instant-date))
 
-(defn find-last-date [xml-content]
+(defn atom-new-posts [xml-content last-updated-date]
+  (->> xml-content
+       :content
+       (find-tag :entry)
+       (filter #(is-recent-post last-updated-date (atom-post-date %)))))
+
+
+(defn get-new-posts [xml-content last-updated-date]
   (case (:tag xml-content)
-    :rss (find-rss-last-date xml-content)
-    :feed (find-atom-last-date xml-content)
+    :rss (rss-new-posts xml-content last-updated-date)
+    :feed (atom-new-posts xml-content last-updated-date)
     (throw (Exception. "Unsupported feed type. Use RSS or Atom XML feed."))))
 
 
 ;; todo this should take post content as well
-(defn notify [email] (println "notifying" email))
+(defn notify [email new-posts] (println "notifying" email))
 
 (defn update-and-notify
   "First notify user by sending email, and then update last-updated-date in DB."
-  [conn email sub-id new-date]
-  (notify email)
-  (d/transact conn {:tx-data [{:db/id sub-id, :sub/last-updated-date new-date}]}))
+  [conn email sub-id new-posts]
+  (notify email new-posts)
+  (def cur-date (tc/to-date (t/now)))
+  (d/transact conn {:tx-data [{:db/id sub-id, :sub/last-updated-date cur-date}]}))
 
 (defn poll-feed
   "Poll a URL, update last updated date in DB and send notification if updated."
@@ -55,7 +70,6 @@
   (let [xml-content (xml/parse feed-url)
         db (d/db conn)
         {sub-id :db/id last-updated-date :sub/last-updated-date} (schema/find-sub db email feed-url)
-        new-updated-date (find-last-date xml-content)]
-    (println sub-id last-updated-date new-updated-date)
-    (when (or (nil? last-updated-date) (== -1 (compare last-updated-date new-updated-date)))
-      (update-and-notify conn email sub-id new-updated-date))))
+        new-posts (get-new-posts xml-content last-updated-date)]
+    (when (< 0 (count new-posts))
+      (update-and-notify conn email sub-id new-posts))))
--- test/datomic/ion/rustic/poller_test.clj
+++ test/datomic/ion/rustic/poller_test.clj
@@ -3,13 +3,15 @@
    [clojure.instant :as instant]
    [clojure.test :as t]
    [datomic.client.api :as d]
+   [clj-time.core :as time]
+   [clj-time.coerce :as tc]
    [datomic.ion.rustic :as rustic]
    [datomic.ion.rustic.edn :as edn]
    [datomic.ion.rustic.poller :as poller]
    [datomic.ion.rustic.schema :as schema]
    [datomic.ion.rustic.db-utils :as db-utils]
    [datomic.ion.rustic.test-fixtures :as tf]
-   [datomic.ion.rustic.utils :as utils]))
+   [datomic.ion.rustic.utils :as u]))
 
 (tf/test-setup)
 
@@ -17,15 +19,21 @@
 
 (def atom-post "test/datomic/ion/rustic/fixtures/atom-post.edn")
 
-(t/deftest find-last-date-tests
+(t/deftest new-post-tests
   (t/testing "works for rss"
-    (t/is (=
-           (-> rss-post edn/read poller/find-last-date)
-           (instant/read-instant-date "2021-04-23T06:14:01.000-00:00"))))
+    (t/testing "when last-updated is nil"
+      (def filtered (-> rss-post edn/read (poller/get-new-posts nil)))
+      (t/is (= (count filtered) 2)))
+    (t/testing "when last-updated is not nil"
+      (def filtered (-> rss-post edn/read (poller/get-new-posts (instant/read-instant-date "2021-04-20T00:00:00.000-00:00"))))
+      (t/is (= (count filtered) 1))))
   (t/testing "works for atom"
-    (t/is (=
-           (-> atom-post edn/read poller/find-last-date)
-           (instant/read-instant-date "2021-11-22T00:00:00.000-00:00")))))
+    (t/testing "when last-updated is nil"
+      (def filtered (-> atom-post edn/read (poller/get-new-posts nil)))
+      (t/is (= (count filtered) 15)))
+    (t/testing "when last-updated is not nil"
+      (def filtered (-> atom-post edn/read (poller/get-new-posts (instant/read-instant-date "2021-04-20T00:00:00.000-00:00"))))
+      (t/is (= (count filtered) 8)))))
 
 (def mem-notify (atom {}))
 
@@ -38,8 +46,12 @@
   (get @mem-notify (keyword email) false))
 
 (defn mock-notify
-  [email]
+  [email _]
   (swap! mem-notify assoc (keyword email) true))
+
+(defn close-to-now? [start-time]
+  (<
+   (time/in-millis (time/interval (tc/from-date start-time) (time/now))) 1000))
 
 (t/deftest poll-feed-tests
   (with-redefs [clojure.xml/parse                edn/read
@@ -52,9 +64,8 @@
         (reset-notify)
         (t/is (not (notified? email)))
         (poller/poll-feed conn email rss-post)
-        (t/is (=
-               (:sub/last-updated-date (schema/find-sub (d/db conn)  email rss-post))
-               (instant/read-instant-date "2021-04-23T06:14:01.000-00:00")))
+        (t/is (close-to-now?
+               (:sub/last-updated-date (schema/find-sub (d/db conn)  email rss-post))))
         (t/is (notified? email)))
       (t/testing "notifies when date was old"
         (let [sub-id (:db/id (schema/find-sub (d/db conn)  email rss-post))
@@ -64,14 +75,12 @@
         (t/is (not (notified? email)))
         (poller/poll-feed conn email rss-post)
         (t/is (notified? email))
-        (t/is (=
-               (:sub/last-updated-date (schema/find-sub (d/db conn)  email rss-post))
-               (instant/read-instant-date "2021-04-23T06:14:01.000-00:00"))))
+        (t/is (close-to-now?
+               (:sub/last-updated-date (schema/find-sub (d/db conn)  email rss-post)))))
       (t/testing "doesn't notify when up to date"
         (reset-notify)
         (t/is (not (notified? email)))
         (poller/poll-feed conn email rss-post)
         (t/is (not (notified? email)))
-        (t/is (=
-               (:sub/last-updated-date (schema/find-sub (d/db conn)  email rss-post))
-               (instant/read-instant-date "2021-04-23T06:14:01.000-00:00")))))))
+        (t/is (close-to-now?
+               (:sub/last-updated-date (schema/find-sub (d/db conn)  email rss-post))))))))
