32a1210e56fa5e55290350037931b0939216f333
Akshat Bubna <akshatb42@gmail.com>
2021-12-19 01:16:15 -0800

    fix lambda tests

--- test/datomic/ion/rustic/lambdas_test.clj
+++ test/datomic/ion/rustic/lambdas_test.clj
@@ -5,18 +5,19 @@
    [clojure.data.json :as json]
    [datomic.ion.rustic.test-fixtures :as tf]
    [datomic.ion.rustic.db-utils :as db-utils]
+   [datomic.ion.rustic.http :as http]
    [datomic.ion.rustic.lambdas :as lambdas]))
 
 (tf/test-setup)
 
 (defn- register-and-get
   [email url]
-  (lambdas/register-sub {:input (json/write-str {:email email, :feed-url url})})
+  (http/register-sub {:body (char-array (json/write-str {:email email, :feed-url url}))})
   (->> {:input (json/write-str email)}
-      (lambdas/get-subs-by-email)
-      edn/read-string
-      (sort-by #(-> % first :sub/feed-url))
-      doall))
+       (lambdas/get-subs-by-email)
+       edn/read-string
+       (sort-by #(-> % first :sub/feed-url))
+       doall))
 
 (t/deftest subs-tests
   (t/testing "register subs works"
