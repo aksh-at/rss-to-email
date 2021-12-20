d68317c99e69f3bc7b0576c5cd37b6db94b0fa58
Akshat Bubna <akshatb42@gmail.com>
2021-12-20 00:13:59 -0800

    rm -rf frontend

--- frontend/styles/boxShadow.ts
+++
@@ -1,8 +0,0 @@
-const boxShadow = Object.freeze({
-  medium: '0px 2px 15px 0px rgba(73, 35, 0, 0.15)',
-  navbar: '0px 0px 6px 2px rgba(73, 35, 0, 0.08)',
-  soft: '0px 2px 10px 0px rgba(73, 35, 0, 0.1)',
-  strong: '0px 2px 15px 1px rgba(73, 35, 0, 0.4)',
-})
-
-export default boxShadow
--- frontend/styles/zIndex.ts
+++
@@ -1,5 +0,0 @@
-const zIndex = {
-  base: 0,
-}
-
-export default zIndex
