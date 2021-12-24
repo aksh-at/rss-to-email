7e1a9d32ba4fedabe9463d5fddf359c00d118767
Akshat Bubna <akshatb42@gmail.com>
2021-12-24 00:51:09 -0800

    fix tests

--- test/datomic/ion/rustic/http_test.clj
+++ test/datomic/ion/rustic/http_test.clj
@@ -27,31 +27,7 @@
    (str/split #">")
    first))
 
-(defn- register-and-get
-  [email url]
-  (http/request-sub {:body (char-array (json/write-str {:email email, :feed-url url}))})
-  ;; (println (get @mem-send-email :body))
-  (def token (get-token))
-  ;; (println token)
-  (http/register-sub {:body (char-array (json/write-str {:token token, :feed-url url}))})
-  (->> {:input (json/write-str email)}
-       (lambdas/get-subs-by-email)
-       edn/read-string
-       (sort-by #(-> % first :sub/feed-url))
-       doall))
-
-(t/deftest subs-tests
-  (with-redefs [datomic.ion.rustic.mailer/send-email mock-send-email]
-    (t/testing "register subs works"
-      (t/is (= (register-and-get "user1" "f1") [[#:sub{:email "user1", :feed-url "f1"}]]))
-      (t/is (= (register-and-get "user1" "f2") [[#:sub{:email "user1", :feed-url "f1"}] [#:sub{:email "user1", :feed-url "f2"}]])))))
-
-(defn- get-subs
-  [email]
-  (http/request-manage {:body (char-array (json/write-str {:email email}))})
-  ;; (println (get @mem-send-email :body))
-  (def token (get-token))
-  ;; (println token)
+(defn- get-subs [token]
   (->>  {:body (char-array (json/write-str {:token token}))}
         http/get-current-subs
         :body
@@ -59,6 +35,29 @@
         :subs
         (sort-by #(-> % first :sub/feed-url))
         doall))
+
+(defn- register-and-get-subs
+  [email url]
+  (http/request-sub {:body (char-array (json/write-str {:email email, :feed-url url}))})
+  ;; (println (get @mem-send-email :body))
+  (def token (get-token))
+  ;; (println token)
+  (http/register-sub {:body (char-array (json/write-str {:token token, :feed-url url}))})
+  (get-subs token))
+
+(t/deftest subs-tests
+  (with-redefs [datomic.ion.rustic.mailer/send-email mock-send-email]
+    (t/testing "register subs works"
+      (t/is (= (register-and-get-subs "user1" "f1") [[#:sub{:email "user1", :feed-url "f1"}]]))
+      (t/is (= (register-and-get-subs "user1" "f2") [[#:sub{:email "user1", :feed-url "f1"}] [#:sub{:email "user1", :feed-url "f2"}]])))))
+
+(defn- request-and-get-subs
+  [email]
+  (http/request-manage {:body (char-array (json/write-str {:email email}))})
+  ;; (println (get @mem-send-email :body))
+  (def token (get-token))
+  ;; (println token)
+  (get-subs token))
 
 (defn- unsub
   [email feed-url]
@@ -69,10 +68,10 @@
 (t/deftest manage-tests
   (with-redefs [datomic.ion.rustic.mailer/send-email mock-send-email]
     (t/testing "manage subs works"
-      (register-and-get "user2" "f1")
-      (register-and-get "user2" "f2")
-      (t/is (= (get-subs "user2") [[#:sub{:email "user2", :feed-url "f1"}] [#:sub{:email "user2", :feed-url "f2"}]]))
-      (register-and-get "user2" "f3")
-      (t/is (= (get-subs "user2") [[#:sub{:email "user2", :feed-url "f1"}] [#:sub{:email "user2", :feed-url "f2"}] [#:sub{:email "user2", :feed-url "f3"}]]))
+      (register-and-get-subs "user2" "f1")
+      (register-and-get-subs "user2" "f2")
+      (t/is (= (request-and-get-subs "user2") [[#:sub{:email "user2", :feed-url "f1"}] [#:sub{:email "user2", :feed-url "f2"}]]))
+      (register-and-get-subs "user2" "f3")
+      (t/is (= (request-and-get-subs "user2") [[#:sub{:email "user2", :feed-url "f1"}] [#:sub{:email "user2", :feed-url "f2"}] [#:sub{:email "user2", :feed-url "f3"}]]))
       (unsub "user2" "f2")
-      (t/is (= (get-subs "user2") [[#:sub{:email "user2", :feed-url "f1"}] [#:sub{:email "user2", :feed-url "f3"}]])))))
+      (t/is (= (request-and-get-subs "user2") [[#:sub{:email "user2", :feed-url "f1"}] [#:sub{:email "user2", :feed-url "f3"}]])))))
